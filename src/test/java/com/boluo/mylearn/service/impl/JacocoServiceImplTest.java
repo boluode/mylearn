5c37ad258335448fa23b15cf1f548693567829f5
李彦龙 <liyanlong1@100tal.com>
2019-10-21 16:27:56 +0800

    add JacocoServiceImplTest

---
+++ src/test/java/com/boluo/mylearn/service/impl/JacocoServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.boluo.mylearn.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JacocoServiceImplTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void subtract() {
+    }
+
+    @Test
+    public void multiply() {
+    }
+
+    @Test
+    public void divide() {
+    }
+}

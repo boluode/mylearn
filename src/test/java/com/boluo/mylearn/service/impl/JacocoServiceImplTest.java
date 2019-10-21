d3a8a66910c2f6ad2662a2ff3fe4b55c6c7c819d
李彦龙 <liyanlong1@100tal.com>
2019-10-21 16:29:24 +0800

    add JacocoServiceImplTest

--- src/test/java/com/boluo/mylearn/service/impl/JacocoServiceImplTest.java
+++ src/test/java/com/boluo/mylearn/service/impl/JacocoServiceImplTest.java
@@ -1,13 +1,18 @@
 package com.boluo.mylearn.service.impl;
 
+import com.boluo.mylearn.service.JacocoService;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class JacocoServiceImplTest {
 
+    private JacocoService jacocoService = new JacocoServiceImpl();
+
     @Test
     public void add() {
+
+        jacocoService.add(1, 1);
     }
 
     @Test
